5c08c04ed5c169d7f397b20664ce517b16b76601
Sygnom1215 <peacepark112@gmail.com>
2021-09-08 22:14:59 +0900

    한승쌤 강의 1시간 4분까지 봤음. 마저 볼 것. ( MonoSIngleton, GameManager(미완))

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monosingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static bool shuttingDown = false;
+    private static object locker = new object();
+
+    private static T instance = null;
+    private static T Instance
+    {
+        get
+        {
+            if(shuttingDown)
+            {
+                Debug.LogWarning("[Instance] Instance " + typeof(T) + " is already destroyed. Returning null.");
+                return null;
+            }
+
+            lock (locker)
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+                    if (instance == null)
+                    {
+                        instance = new GameObject(typeof(T).ToString()).AddComponent<T>();
+                        DontDestroyOnLoad(instance); // 아마 신 변경하는거 만들거면 수정해줘야할것..? 아직은 잘 모르겠다. 
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
