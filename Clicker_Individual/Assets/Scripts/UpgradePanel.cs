30ac5641c609911e4580f167e21595a1a3f4ed11
Sygnom1215 <peacepark112@gmail.com>
2021-09-08 21:26:40 +0900

    스크롤창 만들었음. 스크립트 작성해야돼서 1차시 강의 보러 갈 것

---
+++ Clicker_Individual/Assets/Scripts/UpgradePanel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradePanel : MonoBehaviour
+{
+    //[SerializeField]
+    //private Text itemNameText = null;
+    //[SerializeField]
+    //private Text amountText = null;
+    //[SerializeField]
+    //private Text priceText = null;
+    //[SerializeField]
+    //private Button purchaseButton = null;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
